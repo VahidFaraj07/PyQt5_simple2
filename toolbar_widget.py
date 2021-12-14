a4f9d5a6ddc1a79ef906811fe50565e117cfa9d4
Vahid <93490818+VahidFaraj07@users.noreply.github.com>
2021-12-14 19:30:07 +0400

    toolbar_widget

--- toolbar_widget.py
+++ toolbar_widget.py
@@ -16,9 +16,40 @@ class Window(QMainWindow):
         main_tBar = self.addToolBar("myToolBar")
 
         main_tBar.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
-        subBar_new = QAction(QIcon('logo/folder.png'), 'New', self)
+
+        subBar_new = QAction(QIcon('logo/empty.png'), 'New', self)
         main_tBar.addAction(subBar_new)
         
+        subBar_open = QAction(QIcon('logo/folder.png'), 'Open', self)
+        main_tBar.addAction(subBar_open)
+
+        subBar_save = QAction(QIcon('logo/save.png'), 'Save', self)
+        main_tBar.addAction(subBar_save)
+
+        subBar_exit = QAction(QIcon('logo/exit.png'), 'Exit', self)
+        main_tBar.addAction(subBar_exit)
+        subBar_exit.triggered.connect(self.exitFunc)
+
+        # all action in one func
+        main_tBar.actionTriggered.connect(self.allActionInOne)
+
+
+
+    def exitFunc(self):
+        mes_box = QMessageBox.question(
+            self, "Warning", "Are you sure you want to exit?", QMessageBox.Yes | QMessageBox.No | QMessageBox.No)
+        if mes_box == QMessageBox.Yes:
+            sys.exit()
+
+    def allActionInOne(self, action_type):
+        if action_type.text() == 'New':
+            print('New')
+
+        elif action_type.text() == 'Open':
+            print('Open')
+
+        elif action_type.text() == 'Save':
+            print('Save')
 
 
 def main():
